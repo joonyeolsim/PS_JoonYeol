819e409fff63932986bd5f7cbc0d221261944fef
SimJoonYeol <12195302@inha.edu>
2022-04-05 23:51:40 +0900

    Remove Git Conflict

--- BaekJoon/TwoPointer/1806번 부분합/solve_1806.cpp
+++ BaekJoon/TwoPointer/1806번 부분합/solve_1806.cpp
@@ -6,26 +6,6 @@ int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
     cout.tie(nullptr);
-<<<<<<< HEAD
-    int N, S;
-    vector<int> num(100000);
-    vector<int> cumulative_sum(100000);
-    cin >> N >> S;
-    cin >> num[0];
-    cumulative_sum[0] = num[0];
-    for (int i=1; i<N; i++) {
-        cin >> num[i];
-        cumulative_sum[i] = num[i-1] + num[i];
-    }
-    for (int window=0; window<N; window++){
-        for (int j=0; j<N-window; j++) {
-            if (cumulative_sum[j] - cumulative_sum[j- window - 1] >= S) {
-                cout << window;
-                return 0;
-            }
-        }
-    }
-=======
     int N, S, sum = 0, start = 0, end = 0, min_length = 100001;
     cin >> N >> S;
     vector<int> num(N+1);
@@ -53,5 +33,4 @@ int main() {
     min_length = min_length == 100001 ? 0 : min_length;
     cout << min_length;
     return 0;
->>>>>>> master
 }
