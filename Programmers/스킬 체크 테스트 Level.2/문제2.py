f637690af62a5665d77c195ac9ffc343291ccb2d
SimJoonYeol <12195302@inha.edu>
2022-05-02 17:33:49 +0900

    solve 스킬 체크 테스트 Level.2

---
+++ Programmers/스킬 체크 테스트 Level.2/문제2.py
@@ -0,0 +1,14 @@
+def gcd(a, b):
+    while b > 0:
+        a, b = b, a % b
+    return a
+
+
+def solution(w, h):
+    answer = w * h - (w + h - gcd(w, h))
+
+    return answer
+
+
+if __name__ == '__main__':
+    print(solution(8, 12))
