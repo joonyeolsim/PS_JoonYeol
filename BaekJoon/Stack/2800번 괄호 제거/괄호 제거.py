61833be8d9cf1608792f59eb5d1ae5140f419bda
SimJoonYeol <12195302@inha.edu>
2022-12-29 14:14:46 +0900

    solve 2800 by python

--- BaekJoon/Stack/2800번 괄호 제거/괄호 제거.py
+++ BaekJoon/Stack/2800번 괄호 제거/괄호 제거.py
@@ -1,11 +1,23 @@
+from itertools import combinations
+
 if __name__ == '__main__':
     expression = input()
-    stack = []
+    result = set()
     index_stack = []
+    stack = []
     for i, ch in enumerate(expression):
         if ch == '(':
-            stack.append(i)
+            index_stack.append(i)
         elif ch == ')':
-            stack.pop()
-            
+            stack.append((index_stack.pop(), i))
 
+    for i in range(1, len(stack) + 1):
+        for combination in list(combinations(stack, i)):
+            temp_exp = list(expression[:])
+            for first, second in combination:
+                temp_exp[first] = ''
+                temp_exp[second] = ''
+            result.add(''.join(temp_exp))
+    result = list(result)
+    result.sort()
+    print(*result, sep='\n')
